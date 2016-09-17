742b2cbba89ffb11ca08373f02da7ca987bf395f
Joe Gaffey <joe@joegaffey.com>
2016-09-17 22:23:02 +0100

    Added line sensors and ENA-B pins

--- drone/drone.ino
+++ drone/drone.ino
@@ -3,138 +3,198 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-const int FWD_LEFT = 2;
-const int FWD_RIGHT = 5;
+/* Lego robot pins */
+const int FRONT_LEFT = 2;
+const int FRONT_RIGHT = 5;
 const int BACK_LEFT = 3;
 const int BACK_RIGHT = 4;
 const int LED = 13;
 const int TRIG = 10;
 const int ECHO = 9;
+const int ENA = 11;
+const int ENB = 12;
+const int LINE_SENSORS[] = {6, 7};
 
-const int MIN_DIST = 8;
-const int DEBUG = true;
 
+/* Elegoo robot pins
+const int FRONT_LEFT = 2;
+const int FRONT_RIGHT = 5;
+const int BACK_LEFT = 3;
+const int BACK_RIGHT = 4;
+const int LED = 13;
+const int TRIG = 10;
+const int ECHO = 9;
+const int[] LINE_SENSORS = {6, 7, 8};
+*/
+
+/* Misc settings */
+const int MINIMUM_DISTANCE = 8;
+const int DEBUG_MODE = false;
+const int SERIAL_PORT_SPEED = 9600;
+
+/* Serial messages */
+const int COLLISION_MESSAGE = -1;
+const int LINE_DETECTED_MESSAGE = -2;
+
+/* Globals */
 int distance = -1;
 bool running = true;
 
 void forward() {
-    digitalWrite(FWD_LEFT, HIGH);
-    digitalWrite(FWD_RIGHT, HIGH);
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  digitalWrite(FRONT_LEFT, HIGH);
+  digitalWrite(FRONT_RIGHT, HIGH);
 }
 
 void right() {
-    digitalWrite(BACK_LEFT, HIGH);
-    digitalWrite(FWD_RIGHT, HIGH);
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  digitalWrite(BACK_LEFT, HIGH);
+  digitalWrite(FRONT_RIGHT, HIGH);
 }
 
 void backwards() {
-    digitalWrite(BACK_LEFT, HIGH);
-    digitalWrite(BACK_RIGHT, HIGH);
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  digitalWrite(BACK_LEFT, HIGH);
+  digitalWrite(BACK_RIGHT, HIGH);
 }
 
 void left() {
-    digitalWrite(FWD_LEFT, HIGH);
-    digitalWrite(BACK_RIGHT, HIGH);
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  digitalWrite(FRONT_LEFT, HIGH);
+  digitalWrite(BACK_RIGHT, HIGH);
 }
 
 void stopRobot() {
-    digitalWrite(FWD_LEFT, LOW);
-    digitalWrite(BACK_LEFT, LOW);
-    digitalWrite(BACK_RIGHT, LOW);
-    digitalWrite(FWD_RIGHT, LOW);
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  digitalWrite(FRONT_LEFT, LOW);
+  digitalWrite(BACK_LEFT, LOW);
+  digitalWrite(BACK_RIGHT, LOW);
+  digitalWrite(FRONT_RIGHT, LOW);
 }
 
 float getDistance() {
-    pinMode(TRIG, OUTPUT);
-    digitalWrite(TRIG, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG, HIGH);
-    delayMicroseconds(10);
-    pinMode(ECHO, INPUT);
-    return pulseIn(ECHO, HIGH, 30000)/58.0;
+  pinMode(TRIG, OUTPUT);
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  pinMode(ECHO, INPUT);
+  return pulseIn(ECHO, HIGH, 30000) / 58.0;
+}
+
+boolean lineDetected() {
+  for(int i = 0; i < sizeof(LINE_SENSORS); i++) {
+    if(digitalRead(LINE_SENSORS[i]) == HIGH)
+      return true;
+  }
+  return false;
 }
 
 void ledOn() {
-    digitalWrite(LED, HIGH);
+  digitalWrite(LED, HIGH);
 }
 
 void ledOff() {
-    digitalWrite(LED, LOW);
+  digitalWrite(LED, LOW);
 }
 
-String raw = "";
-int action = -1;
-
-void readAction() {
-    if (Serial.available() > 0) {
-      action = Serial.readStringUntil('\n').toInt();
-    }
-    else
-      action = -1;
-}
-
-void setup() {
-    pinMode(FWD_LEFT, OUTPUT);
-    pinMode(FWD_RIGHT, OUTPUT);
-    pinMode(BACK_LEFT, OUTPUT);
-    pinMode(BACK_RIGHT, OUTPUT);
-    pinMode(LED, OUTPUT);
-    Serial.begin(9600);
+int readActionFromSerial() {
+  if (Serial.available() > 0) 
+    return Serial.readStringUntil('\n').toInt();
+  else
+    return -1;
 }
 
 void ping() {
   Serial.println((int)getDistance());
 }
 
-void checkDistance() {
-  int currDistance = (int)getDistance();
-    if(currDistance == 0 || currDistance == 3) //Filter out false 0's and 3's ???
-      ;
-    else if(currDistance != distance) {
-      distance = currDistance;
-      Serial.println(distance);
-      if(distance < MIN_DIST) {
-        stopRobot();
-        Serial.println(-1);
-        if(!DEBUG)
-          running = false;
-      }
-   }
+void handleAction(int action) {
+  switch(action) {
+      case 0:
+          stopRobot();
+          break;
+      case 1:
+          forward();
+          break;
+      case 2:
+          backwards();
+          break;
+      case 3:
+          left();
+          break;
+      case 4:
+          right();
+          break;
+      case 5:
+          ledOn();
+          break;
+      case 6:
+          ledOff();
+          break;
+      case 7:
+          ping();
+          break;
+      default:
+          break;
+  }
+}
+
+void handleCollision() {
+  stopRobot();
+  Serial.println(COLLISION_MESSAGE);
+  if(!DEBUG_MODE)
+    running = false;
+}
+
+void handleLinedDetected() {
+  stopRobot();
+  Serial.println(LINE_DETECTED_MESSAGE);
+  if(!DEBUG_MODE) 
+    running = false;
+}
+
+void setupLineSensors() {
+  for(int i = 0; i < sizeof(LINE_SENSORS); i++) {
+    pinMode(LINE_SENSORS[i], INPUT);
+  }
+}
+
+void setup() {
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(FRONT_LEFT, OUTPUT);
+  pinMode(FRONT_RIGHT, OUTPUT);
+  pinMode(BACK_LEFT, OUTPUT);
+  pinMode(BACK_RIGHT, OUTPUT);
+  pinMode(LED, OUTPUT);
+  setupLineSensors();       
+  Serial.begin(SERIAL_PORT_SPEED);
 }
 
 void loop() {
-    readAction();
-    if(running && action > -1) {
-        switch(action) {
-            case 0:
-                stopRobot();
-                break;
-            case 1:
-                forward();
-                break;
-            case 2:
-                backwards();
-                break;
-            case 3:
-                left();
-                break;
-            case 4:
-                right();
-                break;
-            case 5:
-                ledOn();
-                break;
-            case 6:
-                ledOff();
-                break;
-            case 7:
-                ping();
-                break;
-            default:
-                break;
-        }
-    }
-    checkDistance();
-    delay(100);
-    action = -1;    
+  int action = readActionFromSerial();
+  if(running && action > -1) 
+    handleAction(action);
+  
+  int tmpDistance = (int)getDistance();
+  if(tmpDistance == 0 || tmpDistance == 3) //Filter out false 0's and 3's - NB: replace that sensor!!! 
+    ;
+  else if(tmpDistance != distance && tmpDistance > 0) { //Only address changes in distance 
+    distance = tmpDistance;
+    Serial.println(distance);
+    if(distance < MINIMUM_DISTANCE)
+      handleCollision();
+  }   
+  
+  if(lineDetected) {
+    handleLinedDetected();
+  }
+  
+  delay(100);
 }
